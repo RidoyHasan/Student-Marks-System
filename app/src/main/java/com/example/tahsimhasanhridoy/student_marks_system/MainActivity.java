c6ecd4230a12fe3e17112fbfc01ead09bfb36de3
Tahsim Hasan Hridoy <ridoyhasan158840@gmail.com>
2019-03-03 00:33:47 +0600

    Initial message

--- app/src/main/java/com/example/tahsimhasanhridoy/student_marks_system/MainActivity.java
+++ app/src/main/java/com/example/tahsimhasanhridoy/student_marks_system/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.tahsimhasanhridoy.student_marks_system;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button loginButton;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton= (Button) findViewById(R.id.loginButtonId);
+        textView= (TextView) findViewById(R.id.textViewId);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Login Button is Clicked");
+            }
+        });
+
+
+
     }
 }
